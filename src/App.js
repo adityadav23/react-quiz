6c7b711bb932eefe9983541df7c4351f87696956
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2024-05-09 10:25:28 +0530

    added reducer hook to manage questions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header'
 import Main from './Main';
 
+const STATUS = {
+  ready:1,
+  loading: 2,
+  error:3,
+  finished: 4,
+  active: 5,
+}
+const initialState = {
+  questions:[],
+  status: STATUS['ready'] // 
+}
+
+function reducer(state, action){
+  switch(action.type){
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: STATUS['ready']
+      }
+    case "dataFailed":
+      return {
+        ...state,
+        status: STATUS['error']
+      }
+    default: 
+      throw new Error("Unknown action")
+  }
+}
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function(){
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({type: "dataReceived",payload: data}))
+      .catch((err) => {
+        dispatch({type: "dataFailed"})
+      });
+  }, []);
+  
   return (
     <div className="app">
       <Header></Header>
